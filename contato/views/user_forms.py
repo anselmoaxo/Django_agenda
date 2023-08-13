300e3b433bc57affd92eea0c413e27b73e607b02
anselmoaxo <anselmo.cstecnologia@gmail.com>
2023-08-13 18:09:17 -0300

    update login e logout

--- contato/views/user_forms.py
+++ contato/views/user_forms.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.contrib import messages
+from django.contrib import messages, auth
 from contato.forms import RegisterForm
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def register(request):
@@ -16,3 +17,30 @@ def register(request):
             return redirect("contato:index")
 
     return render(request, "contato/register.html", {"form": form})
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == "POST":
+        form =AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Logado com sucesso!')
+            return redirect('contato:index')
+        messages.error(request, 'Login inválido')
+
+    return render(
+        request,
+        'contato/login.html',
+        {
+            'form': form
+        }
+    )
+
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contato:login')

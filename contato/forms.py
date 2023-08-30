48bdfcfa62ed346341d67e4f72785680abfaf7d2
Anselmo Xavier <anselmo.cstecnologia@gmail.com>
2023-08-30 20:32:50 -0300

    alteraão form

--- contato/forms.py
+++ contato/forms.py
@@ -1,10 +1,9 @@
 from django import forms
 from django.contrib.auth import password_validation
+from django.core.exceptions import ValidationError
+from . import models
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
-
-from . import models
 
 
 class ContatoForm(forms.ModelForm):
@@ -20,70 +19,53 @@ class ContatoForm(forms.ModelForm):
     class Meta:
         model = models.Contato
         fields = (
-            'first_name', 'last_name', 'phone',
-            'email', 'description', 'category',
-            'picture',
+            "first_name",
+            "last_name",
+            "phone",
+            "email",
+            "description",
+            "category",
+            "picture",
         )
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        first_name = cleaned_data.get('first_name')
-        last_name = cleaned_data.get('last_name')
-
-        if first_name == last_name:
-            msg = ValidationError(
-                'Primeiro nome não pode ser igual ao segundo',
-                code='invalid'
-            )
-            self.add_error('first_name', msg)
-            self.add_error('last_name', msg)
-
-        return super().clean()
-
-    def clean_first_name(self):
-        first_name = self.cleaned_data.get('first_name')
-
-        if first_name == 'ABC':
-            self.add_error(
-                'first_name',
-                ValidationError(
-                    'Veio do add_error',
-                    code='invalid'
-                )
-            )
-
-        return first_name
 
 
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(
         required=True,
-        min_length=3,
+        min_length=10,
+        # error_messages={'required': ' Nome é obrigatorio' }
     )
     last_name = forms.CharField(
         required=True,
-        min_length=3,
+        min_length=10,
+        # error_messages={'required': ' Nome é obrigatorio' }
     )
-    email = forms.EmailField()
+    email = forms.EmailField(
+        required=True,
+        # error_messages={'required': ' Nome é obrigatorio' }
+    )
 
     class Meta:
         model = User
         fields = (
-            'first_name', 'last_name', 'email',
-            'username', 'password1', 'password2',
+            "first_name",
+            "last_name",
+            "email",
+            "username",
+            "password1",
+            "password2",
         )
 
     def clean_email(self):
-        email = self.cleaned_data.get('email')
-
+        email = self.cleaned_data.get("email")
         if User.objects.filter(email=email).exists():
-            self.add_error(
-                'email',
-                ValidationError('Já existe este e-mail', code='invalid')
-            )
-
+            self.add_error("email",
+                           ValidationError("Email já existe ", code="invalid")
+                        )
+            
         return email
-
+            
+    
 
 class RegisterUpdateForm(forms.ModelForm):
     first_name = forms.CharField(
